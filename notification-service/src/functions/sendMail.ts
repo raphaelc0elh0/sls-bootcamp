87be119c5308cd354e8e88cd3a6d8b465f215044
Raphael Coelho <raphael_coelho@hotmail.com.br>
2023-01-08 17:09:14 -0300

    feature: add MailQueue

--- notification-service/src/functions/sendMail.ts
+++ notification-service/src/functions/sendMail.ts
@@ -1,12 +1,37 @@
-import { APIGatewayController } from "@libs/api-gateway";
+import { SQSHandler } from "aws-lambda";
+import { SES } from "aws-sdk";
 
-const _handler: APIGatewayController = async (event) => {
-  console.log(event);
+const ses = new SES({ region: process.env.REGION });
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(event),
-  };
+export const handler: SQSHandler = async (event) => {
+  for (const record of event.Records) {
+    console.log("record processing...", record);
+
+    const email = JSON.parse(record.body);
+    const { subject, body, recipient } = email;
+
+    const params = {
+      Source: "raphael.lima.coelho@gmail.com",
+      Destination: {
+        ToAddresses: [recipient],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: body,
+          },
+        },
+        Subject: {
+          Data: subject,
+        },
+      },
+    };
+
+    try {
+      const result = await ses.sendEmail(params).promise();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
-
-export const handler = _handler;
